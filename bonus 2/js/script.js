98c6c6a8d2cd0664adbcca87c1181c7afd50815b
Mark-dev30 <fronterremarco@gmail.com>
2022-11-29 18:13:12 +0100

    bonus 2 done

--- bonus 2/js/script.js
+++ bonus 2/js/script.js
@@ -4,21 +4,21 @@ for (let i = 1; i<=100; i++){
     let element;
 
     if (i%3 == 0 && i%5 == 0){
-        element = `<div class"box box-pink">${"FizzBuzz"}</div>`
+        element = `<div class="box box-pink">${"FizzBuzz"}</div>`
     }
 
     else if(i%3 == 0){
-        element = `<div class"box box-lightblue">${"Fizz"}</div>`
+        element = `<div class="box box-lightblue">${"Fizz"}</div>`
         
     }
 
     else if(i%5 == 0){
-        element = `<div class"box box-yellow">${"Buzz"}</div>`
+        element = `<div class="box box-yellow">${"Buzz"}</div>`
         
     }
 
     else {
-        element = `<div class"box box-blue">${i}</div>`
+        element = `<div class="box box-blue">${i}</div>`
     }
 
     container.innerHTML += element;
