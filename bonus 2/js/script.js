8fdf8c1f2adf93d00b7d6cb89466bd3826168d9d
Mark-dev30 <fronterremarco@gmail.com>
2022-11-29 16:19:38 +0100

    bonus 2 start

---
+++ bonus 2/js/script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".container");
+
+for (let i = 1; i<=100; i++){
+    let element;
+
+    if (i%3 == 0 && i%5 == 0){
+        element = `<div class"box box-pink">${"FizzBuzz"}</div>`
+    }
+
+    else if(i%3 == 0){
+        element = `<div class"box box-lightblue">${"Fizz"}</div>`
+        
+    }
+
+    else if(i%5 == 0){
+        element = `<div class"box box-yellow">${"Buzz"}</div>`
+        
+    }
+
+    else {
+        element = `<div class"box box-blue">${i}</div>`
+    }
+
+    container.innerHTML += element;
+    
+}
